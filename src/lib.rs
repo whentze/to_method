783f5637416b2f2900ce2663867e2c0abfaa5d4e
sb64 <53383020+sb64@users.noreply.github.com>
2021-05-12 17:33:05 -0600

    Add `try_to` support

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
-//! A utility micro-crate for using [`Into`](::core::convert::Into) more ergonomically.
+//! A utility micro-crate for using [`Into`](::core::convert::Into) and
+//! [`TryInto`](::core::convert::TryInto) more ergonomically.
 //!
-//! It exposes a [`To`](crate::To) extension trait with a [`.to()`](To::to) method
-//! which you can use to invoke [`Into::into`](::core::convert::Into::into)
-//! while specifying the target type and without having to abandon method-call syntax.
+//! It exposes a [`To`](crate::To) extension trait with a [`.to()`](To::to)
+//! method and [`.try_to()`](To::try_to) method which you can use to invoke
+//! [`Into::into`](::core::convert::Into::into) and
+//! [`TryInto`](::core::convert::TryInto::try_into) while specifying the target
+//! type and without having to abandon method-call syntax.
 //!
 //! Being a micro-crate, it tries to be as nice of a dependency as possible and has:
 //!
@@ -38,12 +41,57 @@
 //! // And you can still rely on inference as well:
 //! let z : u32 = y.to();
 //! ```
+//!
+//! # `TryInto`
+//!
+//! The same mechanism works for the `TryInto` trait via the `try_to` method:
+//!
+//! ```
+//! use core::convert::TryInto;
+//!
+//! # fn main() -> Result<(), core::num::TryFromIntError> {
+//!
+//! let x : u16 = 5;
+//!
+//! // The type parameter is on `Into`, not on `Into::into`,
+//! // so we need to do it like this:
+//! let y = TryInto::<u8>::try_into(x)?;
+//!
+//! // Depending on context, inference can make this work though:
+//! let z : u8 = y.try_into()?;
+//!
+//! # Ok(())
+//! # }
+//! ```
+//!
+//! compared to:
+//!
+//! ```
+//! use core::convert::TryInto;
+//!
+//! # fn main() -> Result<(), core::num::TryFromIntError> {
+//!
+//! use to_method::To as _;
+//!
+//! let x : u16 = 5;
+//!
+//! // The type parameter is on the `to` method, so this works:
+//! let y = x.try_to::<u8>()?;
+//!
+//! // And you can still rely on inference as well:
+//! let z : u8 = y.try_to()?;
+//!
+//! # Ok(())
+//! # }
+//! ```
 
 #![no_std]
 #![forbid(missing_docs)]
 #![forbid(unsafe_code)]
 
-/// Extension trait providing the [`to`](To::to) method.
+use core::convert::TryInto;
+
+/// Extension trait providing the [`to`](To::to) and [`try_to`](To::try_to) methods.
 pub trait To {
     /// Converts to `T` by calling `Into<T>::into`.
     #[inline(always)]
@@ -52,6 +100,14 @@ pub trait To {
         Self: Into<T>,
     {
         <Self as Into<T>>::into(self)
+    }
+    
+    /// Tries to convert to `T` by calling `TryInto<T>::try_into`.
+    fn try_to<T>(self) -> Result<T, <Self as TryInto<T>>::Error>
+    where
+        Self: TryInto<T>,
+    {
+        <Self as TryInto<T>>::try_into(self)
     }
 }
 
